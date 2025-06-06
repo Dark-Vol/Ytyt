e8da3c17b43d0d5d828c46e7aa1355249a68180a
Dark-Vol <overlord28985@gmail.com>
2025-06-06 16:27:55 +0300

    Add context menu event listener and script for JivoSite widget in Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react'
 
 export default function Document() {
+  useEffect(()=> {
+    document.addEventListener("contextmenu", e => {
+      console.log(e);
+    })
+  })
   return (
     <Html lang="en">
       <Head />
       <body>
         <Main />
         <NextScript />
+        <script src="//code.jivosite.com/widget/ss8KEh2KiZ" async defer/>
       </body>
     </Html>
-  );
+  )
 }
