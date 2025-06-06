fb76821ba27e171b1611b2789bf8db32834268b7
Dark-Vol <overlord28985@gmail.com>
2025-06-06 13:50:55 +0300

    Add constants for technologies, courses, and student data

---
+++ constant/index.ts
@@ -0,0 +1,772 @@
+export const TECHNOLOGIES_DATA = [
+  {
+    name: "Язык программирования Python",
+    bg: "#5096ff",
+  },
+  {
+    name: "Сети",
+    bg: "#ffb359",
+  },
+  {
+    name: "Базы данных",
+    bg: "#ff6f50",
+  },
+  {
+    name: "Фреймворки Flask и Django",
+    bg: "#4bd071",
+  },
+  {
+    name: "Отладка и тестирование",
+    bg: "#50c0ff",
+  },
+  {
+    name: "Docker",
+    bg: "#4b77b9",
+  },
+  {
+    name: "Git",
+    bg: "#af93ff",
+  },
+];
+
+export const COURSES_DATA = [
+  {
+    img: "https://ytyt.ru/home/img/advantage_1.svg",
+    title: "Огромное количество практики",
+    desc: "Более 500 самостоятельных заданий и 20 полноценных больших проектов",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_2.svg",
+    title: "Современные методики обучения",
+    desc: "Спиральное обучение: погружаемся в материал постепенно, виток за витком",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_3.svg",
+    title: "Простое и понятное изложение",
+    desc: "Вместо заумных терминов – примеры из реального мира",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_4.svg",
+    title: "Гибкий график занятий",
+    desc: "Учитесь в любое время в удобном для вас темпе",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_5.svg",
+    title: "Прямая связь с опытными программистами",
+    desc: "Задавайте вопросы и отправляйте свой код на проверку",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_6.svg",
+    title: "Оплата небольшими частями",
+    desc: "Платите только за тот материал, который сейчас изучаете, а не за весь курс сразу",
+    bg: "#eaf2f5",
+  },
+  {
+    img: "https://ytyt.ru/home/img/advantage_7.svg",
+    title: "Быстрый и легкий возврат",
+    desc: "Если передумаете учиться – вернём деньги за 3 рабочих дня",
+    bg: "#eaf2f5",
+  },
+];
+
+export const PYTHON_DATA = [
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Python ",
+    mark: "5"
+  },
+]
+
+export const JAVASCRIPT_DATA = [
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-121",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-221",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-321",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "5"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-421",
+    language: "Javascript ",
+    mark: "4"
+  },
+
+
+]
+
+export const CSHARP_DATA = [
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "C#",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-121",
+    language: "C#",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-121",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "C#",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Sayfullayev Asadulloh",
+    module: "3",
+    group: "Fspu-221",
+    language: "C#",
+    mark: "4"
+  },
+  {
+    name: "Shukurjonov Avazbek",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Abdumalikov Asliddin",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+  {
+    name: "Ibaydullayev Qobiljon",
+    module: "3",
+    group: "Fspu-221",
+    language: "C# ",
+    mark: "4"
+  },
+
+]
+
+//!  python
+export const PYTON_GROUP = [
+  {
+    id: 1,
+    text: "Fspu-121",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 2,
+    text: "Fspu-221",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 3,
+    text: "Fspu-321",
+    status: ["Изучать"],
+    url: "/students",
+  },
+
+
+];
+
+//! js
+export const JAVA_SCRIPT_GROUP = [
+  {
+    id: 1,
+    text: "Fspu-121",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 2,
+    text: "Fspu-221",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 3,
+    text: "Fspu-321",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 4,
+    text: "Fspu-421",
+    status: ["Изучать"],
+    url: "/students",
+  },
+
+];
+//! C#
+export const C_GROUP = [
+  {
+    id: 1,
+    text: "Fspu-121",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 2,
+    text: "Fspu-221",
+    status: ["Изучать"],
+    url: "/students",
+  },
+
+];
+
+//! suniy
+export const AI_GROUP = [
+  {
+    id: 1,
+    text: "Fspu-121",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 2,
+    text: "Fspu-221",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 3,
+    text: "Fspu-321",
+    status: ["Изучать"],
+    url: "/students",
+  },
+  {
+    id: 4,
+    text: "Fspu-421",
+    status: ["Изучать"],
+    url: "/students",
+  },
+];
+
+export const EDUC = [
+  {
+    title: "Python",
+    about:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptas rerum maxime ducimus unde. Ad accusantium ratione eius tempore, blanditiis est amet odio reiciendis eos praesentium quidem? Quibusdam, deleniti molestiae a quos saepe minus suscipit quaerat quidem in pariatur libero nam modi quo ea ipsam, repudiandae vel debitis delectus totam!",
+    id: Math.random() * 100000,
+  },
+  {
+    title: "Javascript",
+    about: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio voluptas rerum maxime ducimus unde. Ad accusantium ratione eius tempore, blanditiis est amet odio reiciendis eos praesentium quidem? Quibusdam, deleniti molestiae a quos saepe minus suscipit quaerat quidem in pariatur libero nam modi quo ea ipsam, repudiandae vel debitis delectus totam!",
+    id: Math.random() * 1000000
+  },
+  {
+    title: "C#",
+    id: 300099
+  }
+];
+
+export const ADDS = [
+  {
+    proff: "Frontend",
+    loc: "Tashkent",
+    educ: "BA",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 1,
+    top: 1,
+  },
+  {
+    proff: "Mobile",
+    loc: "Qashqadaryo",
+    educ: "CiscoLive",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 4,
+    top: 2,
+  },
+  {
+    proff: "Flutter",
+    loc: "Sirdaryo",
+    educ: "PDP",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 2,
+    top: 3,
+  },
+  {
+    proff: "Backend",
+    loc: "Namangan",
+    educ: "InTech",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 5,
+    top: 4,
+  },
+  {
+    proff: "Telegram Bot",
+    loc: "Samarqand",
+    educ: "UNITE",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 3,
+    top: 5,
+  },
+  {
+    proff: "React",
+    loc: "Farg'ona",
+    educ: "Bright",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 6,
+    top: 6,
+  },
+  {
+    proff: "Web",
+    loc: "Andijon",
+    educ: "WebDevs",
+    about:
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum, sapiente.",
+    id: 7,
+    top: 7,
+  },
+];
